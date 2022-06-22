bb7c2cb261542505743cf2461f7e5ccc16e31058
Luciano <lucianosuarez701@gmail.com>
2022-06-22 12:53:38 -0300

    personalizacion de admin

--- app_product/admin.py
+++ app_product/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from app_product.models import Celulares, Heladeras, Televisores
 
 # Register your models here.
-admin.site.register(Celulares)
-admin.site.register(Heladeras)
-admin.site.register(Televisores)
+
+@admin.register(Celulares)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'inches', "description", "SKU", "is_active"]
+
+
+@admin.register(Heladeras)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'litters', "description", "SKU", "is_active"]
+
+@admin.register(Televisores)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'inches', "description", "SKU", "is_active"]
